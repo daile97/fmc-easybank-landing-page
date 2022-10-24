a1dd0b2ff7a23d15f4c228f159814460de6cb540
daile97 <daile97.dev@gmail.com>
2022-10-24 22:57:29 +0700

    formatting

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
-const toggledElement = document.querySelectorAll('.toggled')
-const menuBtns = document.querySelectorAll('.menu-btn')
+const toggledElement = document.querySelectorAll(".toggled");
+const menuBtns = document.querySelectorAll(".menu-btn");
 
 const onClicked = (elements, className) => {
-  elements.forEach(el => {
-    el.classList.toggle(className)
-  })
-}
+  elements.forEach((el) => {
+    el.classList.toggle(className);
+  });
+};
 
-menuBtns.forEach(btn => {
-  btn.addEventListener('click', () => onClicked(toggledElement, 'hidden'))
-})
+menuBtns.forEach((btn) => {
+  btn.addEventListener("click", () => onClicked(toggledElement, "hidden"));
+});
